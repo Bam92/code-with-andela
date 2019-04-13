23438b233be20ff7aea047354d05b7f4e533b330
abelmbula <abelmbula@bam.com>
2019-04-13 15:06:23 +0200

    Add test for divisors function

---
+++ tdd/spec/divisorsSpec.js
@@ -0,0 +1,9 @@
+describe('Divisors', function() {
+   it('it returns an array of divisors', function() {
+      expect(divisors(10)).toEqual([2,5]);
+   });
+   
+   /*it('says hello to someone', function() {
+      expect(hello('Abel')).toEqual('Hello, Abel!');
+   });*/
+});
---
+++ tdd/src/divisors.js
@@ -0,0 +1,7 @@
+/*
+ * 
+ */
+
+function divisors(integer) {
+
+}
